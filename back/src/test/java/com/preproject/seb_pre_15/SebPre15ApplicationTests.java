27f214c71b817addfeeb453645b97addb9c50db7
Explorers12 <gva05134@naver.com>
2023-08-09 15:23:45 +0900

    테스트

---
+++ back/src/test/java/com/preproject/seb_pre_15/SebPre15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preproject.seb_pre_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebPre15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
