a784c298c49b6c21f3ae464db8c2ef3d00175605
Explorers12 <gva05134@naver.com>
2023-08-09 14:36:39 +0900

    헤더와풋터

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Style from 'styled-components';
+import Header from './component/header';
+import Footer from './component/footer';
 
-function App() {
+const App:React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main className="flex flex-auto min-h-[calc(100vh-180px)]">
+        <Routes>
+        <Route path="/" element={<Main />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
