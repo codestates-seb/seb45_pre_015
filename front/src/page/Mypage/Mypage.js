142ee43b2a42bafa44f7365a4d02d65d5fb2c82a
Jun Seong <drawbeat0112@gmail.com>
2023-08-18 16:16:58 +0900

    feat/Mypage axios 중간수정

--- front/src/page/Mypage/Mypage.js
+++ front/src/page/Mypage/Mypage.js
@@ -1,12 +1,33 @@
 import React from "react"
+import axios from "axios"
 import Activity from '../../component/Mypage/Activity'
 import Settings from '../../component/Mypage/Settings'
-import { useState } from "react"
+import { useState , useEffect } from "react"
 import { Section } from './Mypage.styled'
 
 
 export default function Mypage() {
+  
+  const [userInfo, setUserInfo] = useState(null);
   const [addClass, setAddClass] = useState(true);
+
+  const dataHanlder = () => {
+    return axios
+    .get("http://localhost:8080//members/mypage")
+    .then((res) => {
+      setUserInfo(res.data)
+    })
+    .catch((err) => {
+      if(err.response.status === 401){
+        console.log(err.response.data);
+      }
+    })
+  }
+
+  useEffect(() => {
+    dataHanlder()
+  }, []);
+
   return (
     <Section>
       <div className="profile">
@@ -17,9 +38,8 @@ export default function Mypage() {
           <h2 className="profile_user_name">유저네임</h2>
           <div className="profile_user_state">
             <button className="logout-btn">Logout</button>
-
           </div>
-        </div>
+        </div>ㄴ
       </div>
       <div className="mypage_status">
         <div className="status_button">
@@ -27,7 +47,7 @@ export default function Mypage() {
           <button onClick={() => setAddClass(false)} className={addClass ? "" : "active"}>Settings</button>
         </div>
         <div className="state_main_wrap">
-          { addClass ? <Activity/> : <Settings />}
+          { addClass ? <Activity userInfo={userInfo}/> : <Settings />}
         </div>
       </div>
     </Section>
