29bc3421b4acbfd7b1672cacb8dff216dd732b42
Explorers <129934405+Explorers12@users.noreply.github.com>
2023-08-22 15:58:09 +0900

    Add files via upload

---
+++ front/src/page/main.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Sidebar from "../component/Sidebar";
+import Aside from "../component/aside";
+import { styled } from "styled-components";
+import TopQuestionList from "../question/toppuestion";
+
+const MainContainerWrap = styled.section`
+  display: flex;
+  max-width: 78.75rem;
+  margin: 0 auto;
+`
+
+const MainContainer = styled.div`
+  display: flex;
+  width: calc(100% - 200px);
+`;
+
+const QuestionListContainer = styled.div`
+  width: 70%;
+`;
+
+const AsideContainer = styled.div`
+  width: 30%;
+`;
+
+function Main() {
+  return (
+    <MainContainerWrap>
+      <Sidebar />
+      <MainContainer>
+        <QuestionListContainer>
+          <TopQuestionList />
+        </QuestionListContainer>
+        <AsideContainer>
+          <Aside />
+        </AsideContainer>
+      </MainContainer>
+    </MainContainerWrap>
+  );
+}
+
+export default Main;
