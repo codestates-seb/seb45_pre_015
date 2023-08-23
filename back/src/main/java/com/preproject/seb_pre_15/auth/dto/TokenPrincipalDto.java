01a1a26255461303c04dbb3cb86c1d6f0c5372b3
SEB_BE_45_전우현 <tbvjdngus@gmail.com>
2023-08-23 11:01:58 +0900

    "Server codes refactoring"

--- back/src/main/java/com/preproject/seb_pre_15/auth/dto/TokenPrincipalDto.java
+++ back/src/main/java/com/preproject/seb_pre_15/auth/dto/TokenPrincipalDto.java
@@ -1,13 +1,16 @@
-package com.preproject.seb_pre_15.auth.dto;
+//package com.preproject.seb_pre_15.auth.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//public class TokenPrincipalDto {
+//    private Long id;
+//    private String email;
+//}
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class TokenPrincipalDto {
-    private Long id;
-    private String email;
-}
+//사용하지 않음
